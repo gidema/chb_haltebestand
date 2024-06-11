157aa379728a4a3fac1252200cc4e8bedf42f96d
Gertjan Idema <devel@gertjanidema.nl>
2024-06-11 10:43:43 +0200

    Fixed classpath and module-info issues

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,9 @@
  */
 module chbhaltebestand {
     requires transitive jakarta.xml.bind;
-//    requires jakarta.xml;
     requires jakarta.activation;
     requires java.base;
     requires java.xml;
-//    requires java.xml;
     exports nl.chb;
     exports nl.chb.binding;
     
